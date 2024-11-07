4c302f7c16e42b2592ab160ff29395526deb2f2e
MrCreepTon <53111789+MrCreepTon@users.noreply.github.com>
2024-11-07 22:16:07 +0300

    lan

--- remote_service_app/vite.config.ts
+++ remote_service_app/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: '192.168.1.7',
     port: 3000,
     proxy: {
       "/api": {
