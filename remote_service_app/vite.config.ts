45693d86155bd599b3068fca201deb2f8cbca7f6
MrCreepTon <53111789+MrCreepTon@users.noreply.github.com>
2024-11-22 15:08:50 +0300

    clear search + tauri fix

--- remote_service_app/vite.config.ts
+++ remote_service_app/vite.config.ts
@@ -4,15 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: '192.168.1.7',
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://192.168.1.7:8000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      }
-    }
+    host: '192.168.1.8',
+    port: 5173
   },
   plugins: [react()],
 })
